498c8f3083813569e9c9f61c838db65881758ee0
j.c.sackett <j.c.sackett@gmail.com>
2014-08-26 23:11:46 +0000

    Register school model with admin.

--- schools/admin.py
+++ schools/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from school_inspector.schools.models import School 
 
-# Register your models here.
+class SchoolAdmin(admin.ModelAdmin):
+        pass
+
+admin.site.register(School, SchoolAdmin)
