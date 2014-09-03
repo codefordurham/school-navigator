c860c5ca4ba5dd32390ae52e4c51b387cdeb30e8
Alex Lemann <lemanal@caktusgroup.com>
2014-09-03 15:35:36 -0700

    Prefix API urls with api/

--- school_inspector/urls.py
+++ school_inspector/urls.py
@@ -9,6 +9,6 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^schools/', include(school_urls)),
+    url(r'^api/schools/', include(school_urls)),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
