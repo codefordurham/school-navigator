1592ba8b6a7e5f9bf5016327cf821a1eec4372b1
Alex Lemann <lemanal@caktusgroup.com>
2014-10-09 17:45:54 -0400

    First attempt at assigned/option/all schools API

--- schools/urls.py
+++ schools/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, url
 
-from schools.views import LocationEligibleSchools
+from schools.views import AssignedSchools, OptionSchools, AllSchools
 
 
 school_urls = patterns('',
-    url(r'eligible/$', LocationEligibleSchools.as_view(), name='eligible-schools')
+    url(r'assigned/$', AssignedSchools.as_view(), name='assigned-schools'),
+    url(r'option/$', OptionSchools.as_view(), name='option-schools'),
+    url(r'all', AllSchools.as_view(), name='all-schools')
 )
--- schools/views.py
+++ schools/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.contrib.gis.geos import Point
+from django.db.models import Q
 
 from rest_framework import generics
 from rest_framework.exceptions import ParseError
@@ -7,18 +7,49 @@ from rest_framework.exceptions import ParseError
 from schools.serializers import SchoolSerializer
 from schools import models as schools_models
 
-class LocationEligibleSchools(generics.ListAPIView):
+def parse_latlon(request):
+    try:
+        lat = request.GET['latitude']
+        lon = request.GET['longitude']
+        pt = Point(float(lon), float(lat))
+    except ValueError:
+        raise ParseError("Bad location")
+    except KeyError:
+        raise ParseError("No location provided")
+    return pt
+
+class AssignedSchools(generics.ListAPIView):
+    """
+    pt is in school districts or walkzones
+    """
     model = schools_models.School
     serializer_class = SchoolSerializer
 
     def get_queryset(self):
-        queryset = super(LocationEligibleSchools, self).get_queryset()
-        try:
-            lat = self.request.GET['latitude']
-            lon = self.request.GET['longitude']
-            pt = Point(float(lon), float(lat))
-        except ValueError:
-            raise ParseError("Bad location")
-        except KeyError:
-            raise ParseError("No location provided")
-        return queryset.filter(district__contains=pt)
+        queryset = super(AssignedSchools, self).get_queryset()
+        pt = parse_latlon(self.request)
+        return queryset.filter(Q(district__contains=pt) | Q(walk_zone__contains=pt))
+
+class OptionSchools(generics.ListAPIView):
+    """
+    Magnets (except where pt is in Walkzone), Charters
+    """
+    model = schools_models.School
+    serializer_class = SchoolSerializer
+
+    def get_queryset(self):
+        queryset = super(OptionSchools, self).get_queryset()
+        pt = parse_latlon(self.request)
+        return queryset.filter(district=None).exclude(walk_zone__contains=pt)
+
+class AllSchools(generics.ListAPIView):
+    """
+    Both Assigned And Option Schools
+    """
+    model = schools_models.School
+    serializer_class = SchoolSerializer
+
+    def get_queryset(self):
+        queryset = super(AllSchools, self).get_queryset()
+        pt = parse_latlon(self.request)
+        return queryset.filter(district=None).exclude(walk_zone__contains=pt)
