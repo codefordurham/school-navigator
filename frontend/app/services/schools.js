28bddac78775339dbb48703c35959e155622c00c
Victor Rocha <victor@rochapps.com>
2014-10-10 09:21:23 -0400

    Move marker to new location.

--- frontend/app/services/schools.js
+++ frontend/app/services/schools.js
@@ -1,10 +1,10 @@
 angular.module('SchoolsApp.services', [])
     .service('Schools', function($http) {
-    this.get = function(location) {
-      console.log(location);
+    this.get = function(location, type) {
+      var url = 'https://schools.codefordurham.com/api/schools/' + type + '/';
       return $http({
           method: 'GET',
-          url: 'https://schools.codefordurham.com/api/schools/eligible/',
+          url: url,
           params: {
               longitude: location.longitude,
               latitude: location.latitude
