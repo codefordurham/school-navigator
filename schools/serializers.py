b043cbdc1e2bcf09ea6e98ecd24647e67537a8d3
Alex Lemann <lemanal@caktusgroup.com>
2014-10-10 10:00:14 -0400

    "type" -> "eligibility"

--- schools/serializers.py
+++ schools/serializers.py
@@ -4,13 +4,13 @@ from rest_framework_gis import serializers as geo_serializers
 import schools.models as schools_models
 
 class SchoolSerializer(geo_serializers.GeoModelSerializer):
-    type = serializers.SerializerMethodField('get_type')
+    eligibility = serializers.SerializerMethodField('get_eligibility')
 
     class Meta:
         model = schools_models.School
         #TODO Add back district?
-        fields = ('id', 'name', 'level', 'address', 'magnet', 'type')
+        fields = ('id', 'name', 'level', 'address', 'magnet', 'eligibility')
 
-    def get_type(self, obj):
+    def get_eligibility(self, obj):
        import random
        return random.choice(('assigned', 'option', 'all'))
