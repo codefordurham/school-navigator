24545ea928217af06f03812d3af1d8b0a8ddd461
Victor Rocha <victor@rochapps.com>
2014-10-10 13:47:20 -0400

    Fixes problem with accessing content from http site

--- frontend/app/directives/MapDirectives.js
+++ frontend/app/directives/MapDirectives.js
@@ -5,11 +5,13 @@ angular.module('SchoolsApp.directives', [])
             var map = L.map('map', { zoomControl:false }).setView([36.002453, -78.905869], 13),
                 marker,
                 markerLatLng,
-                schools_layers = [];
+                schools_layers = [],
+                homeIcon = L.divIcon({className: 'fa fa-home fa-2x', iconSize: '64px'}),
+                schoolIcon = L.divIcon({className: 'fa fa-building fa-2x', iconSize: '64px'});
             element.css({
                 "height": document.documentElement.clientHeight + "px"
             });
-            L.tileLayer('http://{s}.tiles.mapbox.com/v3/vrocha.j3fib8g6/{z}/{x}/{y}.png', {
+            L.tileLayer('https://{s}.tiles.mapbox.com/v3/vrocha.j3fib8g6/{z}/{x}/{y}.png', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 maxZoom: 18
             }).addTo(map);
@@ -28,7 +30,10 @@ angular.module('SchoolsApp.directives', [])
                     markerLatLng = new L.LatLng(location.latitude, location.longitude);
                     marker.setLatLng(markerLatLng);
                 } else {
-                    marker = L.marker([location.latitude, location.longitude]).addTo(map);
+                    marker = L.marker(
+                        [location.latitude, location.longitude],
+                        {icon: homeIcon}
+                    ).addTo(map);
                 }
                 // center marker
                 map.setView([location.latitude, location.longitude], 12);
@@ -39,13 +44,11 @@ angular.module('SchoolsApp.directives', [])
                 clearMap();
                 angular.forEach(schools, function(school) {
                     console.log(school);
-                    var school_layer;
+                    var school_layer,
+                        school_marker =  L.marker([school.location.coordinates['1'], school.location.coordinates['0']], {icon: schoolIcon})
+                            .bindPopup(school.name);
 
-                    school_layer = L.layerGroup([
-                        L.marker([school.location.coordinates['1'], school.location.coordinates['0']])
-                         .bindPopup(school.name)
-                    ]);
-
+                    school_layer = L.layerGroup([school_marker]);
                     schools_layers.push(school_layer);
                     school_layer.addTo(map);
                 });
