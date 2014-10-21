75d12ec977631fbed4ca1b1ce68ccfba197dabc3
Alex Lemann <lemanal@caktusgroup.com>
2014-10-21 19:41:25 -0400

    New preference field

--- schools/serializers.py
+++ schools/serializers.py
@@ -5,10 +5,11 @@ import schools.models as schools_models
 
 class SchoolListSerializer(geo_serializers.GeoModelSerializer):
     eligibility = serializers.SerializerMethodField('get_eligibility')
+    preference = serializers.SerializerMethodField('get_preference')
 
     class Meta:
         model = schools_models.School
-        fields = ('id', 'name', 'level', 'address', 'type', 'eligibility', 'location')
+        fields = ('id', 'name', 'level', 'address', 'type', 'eligibility', 'location', 'preference')
 
     def get_eligibility(self, obj):
         pt = self.context['point']
@@ -22,6 +23,13 @@ class SchoolListSerializer(geo_serializers.GeoModelSerializer):
         if obj.type == 'charter':
             return 'option'
         return 'all'
+
+    def get_preference(self, obj):
+        pt = self.context['point']
+        if obj.priority_zone is not None and obj.priority_zone.contains(pt):
+            return 'priority'
+        if obj.choice_zone is not None and obj.choice_zone.contains(pt):
+            return 'choice'
 
 class SchoolDetailSerializer(geo_serializers.GeoModelSerializer):
     class Meta:
