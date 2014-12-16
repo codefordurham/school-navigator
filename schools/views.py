cf8125e55451d3558d90b2040e5432ef0aa57b3f
Alex Lemann <lemanal@caktusgroup.com>
2014-12-16 17:42:30 -0500

    Clean cases.

--- schools/views.py
+++ schools/views.py
@@ -39,11 +39,10 @@ class AllSchools(SchoolAPIView):
     """
 
     def get_queryset(self):
-        queryset = super(AllSchools, self).get_queryset()
-        queryset.filter(
-                Q(district__contains=self.pt) |
-                Q(type='magnet') |
-                Q(type='specialty') |
-                Q(type='charter')
+        qs = super(AllSchools, self).get_queryset()
+        qs = qs.filter(
+                (~Q(district=None) & Q(district__contains=self.pt)) |
+                (~Q(district=None) & Q(walk_zone__contains=self.pt)) |
+                Q(type__in=('magnet', 'speciality', 'charter'))
         )
-        return queryset
+        return qs
