9f51b29df20b0b9de1bcb29884e4dcd7d5e6c1bb
Jon Anderson <squiggs.jonny5@gmail.com>
2015-04-13 03:46:40 -0400

    More organizational work

--- get_data/get_school-year_data.py
+++ get_data/get_school-year_data.py
@@ -7,6 +7,27 @@ http = httplib2.Http()
 
 base_url = "http://www.ncschoolreportcard.org/src/"
 
+def get_string(x):
+    return x
+
+def get_integer(x):
+    if x == "N/A":
+        return -1
+    else:
+        return int(x)
+
+def p2f(x):
+    if x == "N/A":
+        return -1
+    else:
+        return round(float(x.strip('%'))/100, 3)
+
+convert_data = {
+        "string" : get_string,
+        "integer" : get_integer,
+        "percent" : p2f
+        }
+
 def main():
     data = {}
     pages = { 1: { "key": "size", "function": page1 },
@@ -25,7 +46,7 @@ def main():
               14: { "key": "", "function": page14 },
               15: { "key": "", "function": page15 } }
 
-    for page in range(1,2):
+    for page in range(1,3):
         schoolCode = '304'
         year = '2012-13'
         url = base_url + 'schDetails.jsp?Page=' + str(page) + '&pSchCode=' + schoolCode + '&pLEACode=320&pYear=' + year
@@ -36,25 +57,76 @@ def main():
     pp = pprint.PrettyPrinter(depth=6)
     pp.pprint(data)
 
-def page1(soup):
-    object = { "avg": {},
-               "total" : int(soup.find_all('table',summary="school size")[1].find('tr').find_all('td')[1].text.strip()) }
-    rows = soup.find('table',summary="average class size").find_all('tr')
+def singleHeadTable(rows, type):
+    object = {}
     for index in range(0, len(rows[0].find_all('th'))):
-        grade = rows[0].find_all('th')[index].text.strip()
-        size = rows[1].find_all('td')[index].text.strip()
-        object['avg'][grade] = int(size)
+        head = rows[0].find_all('th')[index].text.strip()
+        body = rows[1].find_all('td')[index].text.strip()
+        object[head] = convert_data[type](body)
     return object
 
-def page2(schoolCode, year):
-    object = { "grades": {}, "groups": { "races": {}, "sexes": {}, "economic": {} } }
-    table = soup.find('table',summary="performance of students in each grade on the ABCs end-of-grade tests")
-    rows = table.find_all('tr')
+def demographicTable(rows, type):
+    object = {
+            "sex": {},
+            "ethnicity": {},
+            "economics": {},
+            "other": {}
+            }
     for index in range(0, len(rows[0].find_all('th'))):
-        grade = rows[0].find_all('th')[index].text.strip()
-        size = rows[1].find_all('td')[index].text.strip()
-        object['avg'][grade] = size
+        body = rows[1].find_all('td')[index].text.strip()
+        if index == 0:
+            object["All"] = convert_data[type](body)
+        elif index == 1:
+            object["sex"]["Male"] = convert_data[type](body)
+        elif index == 2:
+            object["sex"]["Female"] = convert_data[type](body)
+        elif index == 3:
+            object["ethnicity"]["White"] = convert_data[type](body)
+        elif index == 4:
+            object["ethnicity"]["Black"] = convert_data[type](body)
+        elif index == 5:
+            object["ethnicity"]["Hispanic"] = convert_data[type](body)
+        elif index == 6:
+            object["ethnicity"]["American Indian"] = convert_data[type](body)
+        elif index == 7:
+            object["ethnicity"]["Asian"] = convert_data[type](body)
+        elif index == 8:
+            object["ethnicity"]["Pacific Islander"] = convert_data[type](body)
+        elif index == 9:
+            object["ethnicity"]["Multiple"] = convert_data[type](body)
+        elif index == 10:
+            object["economics"]["Disadvantaged"] = convert_data[type](body)
+        elif index == 11:
+            object["economics"]["Not Disadvantaged"] = convert_data[type](body)
+        elif index == 12:
+            object["other"]["Limited English"] = convert_data[type](body)
+        elif index == 13:
+            object["other"]["Migrant"] = convert_data[type](body)
+        elif index == 14:
+            object["other"]["Disabilities"] = convert_data[type](body)
     return object
+
+def page1(soup):
+    rows = soup.find('table',summary="average class size").find_all('tr')
+    object = { "avg": singleHeadTable(rows, "integer"),
+               "total" : int(soup.find_all('table',summary="school size")[1].find('tr').find_all('td')[1].text.strip()) }
+    return object
+
+def page2(soup):
+    ESEArows = soup.find('table',summary="End-of-Grade Science Tests for ESEA").find_all('tr')
+    EOGrows = soup.find('table',summary="percentage of each student group on the North Carolina end-of-grade tests").find_all('tr')
+    object = { "ESEA": singleHeadTable(ESEArows, "percent"),
+            "EOG": demographicTable(EOGrows, "percent") }
+    return object
+
+#    object = { "grades": {}, "groups": { "races": {}, "sexes": {}, "economic": {} } }
+#    table = soup.find('table',summary="performance of students in each grade on the ABCs end-of-grade tests")
+#    rows = table.find_all('tr')
+#    for index in range(0, len(rows[0].find_all('th'))):
+#        grade = rows[0].find_all('th')[index].text.strip()
+#        size = rows[1].find_all('td')[index].text.strip()
+#        object['avg'][grade] = size
+#    return object
 
 def page3(schoolCode, year):
     object = { "avg": {} }
