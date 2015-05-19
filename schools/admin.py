c651a0092d67f7847a8b510b74783759f8f68133
Victor Rocha <victor@rochapps.com>
2015-05-19 18:52:24 -0400

    [master] improve admin for manual data entry

--- schools/admin.py
+++ schools/admin.py
@@ -4,9 +4,14 @@ from leaflet.admin import LeafletGeoAdmin
 
 from .models import School
 
+
 class SchoolAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'level', 'type', 'year_round')
+    list_display = ('name', 'level', 'has_mission_statement', 'photo', 'hours', 'type', 'year_round')
     ordering = ('name',)
     list_filter = ('year_round', 'level', 'type')
+
+    def has_mission_statement(self, obj):
+        if obj.description:
+            return True
 
 admin.site.register(School, SchoolAdmin)
