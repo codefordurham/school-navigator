340e6d27249c6b949fd57c1ca7b559d7a2a86da6
Victor Rocha <victor@rochapps.com>
2015-05-19 19:03:18 -0400

    [master] More admin improvements

--- schools/admin.py
+++ schools/admin.py
@@ -6,12 +6,14 @@ from .models import School
 
 
 class SchoolAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'level', 'has_mission_statement', 'photo', 'hours', 'type', 'year_round')
+    list_display = ('name', 'level', 'has_mission_statement', 'photo', 'school_hours', 'type', 'year_round')
     ordering = ('name',)
     list_filter = ('year_round', 'level', 'type')
 
     def has_mission_statement(self, obj):
         if obj.description:
             return True
+        return False
+    has_mission_statement.boolean = True
 
 admin.site.register(School, SchoolAdmin)
