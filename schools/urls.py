17a66433602375798dbbc71e483eeb4f22f8e21c
Alex Lemann <lemanal@caktusgroup.com>
2015-05-19 19:56:58 -0400

    Only return Active schools to frontend

--- schools/urls.py
+++ schools/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-from schools.views import AllSchools, SchoolDetail
+from schools.views import ActiveSchools, SchoolDetail
 
 
 school_urls = patterns('',
     url(r'detail/(?P<pk>[0-9]+)/$', SchoolDetail.as_view(), name='school-detail'),
-    url(r'$', AllSchools.as_view(), name='all-schools'),
+    url(r'$', ActiveSchools.as_view(), name='all-schools'),
 )
