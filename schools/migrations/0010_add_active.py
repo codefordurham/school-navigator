3031585b3dfd1dd919ab2c53ecdf1a7b10ef1ecf
Alex Lemann <lemanal@caktusgroup.com>
2015-05-19 20:09:08 -0400

    Syncing migrations with victor

--- schools/migrations/0010_add_active.py
+++ schools/migrations/0010_add_active.py
@@ -8,7 +8,7 @@ import django.contrib.gis.db.models.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('schools', '0007_add_desc_photo'),
+        ('schools', '0009_auto_20150519_1903'),
     ]
 
     operations = [
