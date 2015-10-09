3a58ea2ae68748b74f42d6a0d39615245e29b592
Colin Copeland <copelco@caktusgroup.com>
2015-10-09 11:06:33 -0400

    rename inspector to navigator

--- school_navigator/celery.py
+++ school_navigator/celery.py
@@ -7,9 +7,9 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_inspector.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_navigator.settings')
 
-app = Celery('school_inspector')
+app = Celery('school_navigator')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
