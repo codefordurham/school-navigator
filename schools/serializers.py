dc6b6327f72eef4bfe1c4cea6c34126653b4dca9
Colin Copeland <copelco@caktusgroup.com>
2015-10-20 19:04:20 -0400

    remove bad line

--- schools/serializers.py
+++ schools/serializers.py
@@ -45,7 +45,6 @@ class SchoolListSerializer(geo_serializers.GeoModelSerializer):
         pt = self.context['point']
         # return auto-assigned (walk/choice) before other zones
         # see https://github.com/codefordurham/school-navigator/pull/217
-        the order is important - see https://github.com/codefordurham/school-navigator/pull/217
         if obj.district is not None and obj.district.contains(pt):
             return 'neighborhood'
         if obj.walk_zone is not None and obj.walk_zone.contains(pt):
