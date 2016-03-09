a8d59c57a2a84ef778e566754814ecdf80801d3f
Alex Lemann <lemanal@caktusgroup.com>
2016-03-08 19:46:33 -0500

    find the survey. 1 day to fill it out.

--- schools/views.py
+++ schools/views.py
@@ -1,6 +1,19 @@
+from datetime import datetime, timedelta
+
 from schools import forms as schools_forms
+from schools import models as schools_models
+
 from django.shortcuts import render
+from django.http import Http404
 
 def survey_form(request, hash):
-    form = schools_forms.SchoolProfileForm()
+    try:
+        pk = schools_models.SchoolProfile.decode_url(hash)
+    except:
+        raise Http404("Survey does not exist")
+    profile = schools_models.SchoolProfile.objects.get(pk=pk)
+    if profile.submitted_at and \
+            profile.submitted_at > datetime.datetime.now() + timedelta(days=1):
+        raise Http404("Survey Already Completed")
+    form = schools_forms.SchoolProfileForm(instance=profile)
     return render(request, 'survey_form.html', {'survey_form': form})
