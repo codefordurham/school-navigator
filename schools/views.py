e1331ec970349537183a7698a2f9f6078b444a91
Alex Lemann <lemanal@caktusgroup.com>
2016-03-08 20:52:10 -0500

    Fixes survey saving

--- schools/views.py
+++ schools/views.py
@@ -1,10 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from schools import forms as schools_forms
 from schools import models as schools_models
 
 from django.shortcuts import render
 from django.http import Http404
+from django.utils import timezone
 
 def survey_form(request, hash):
     try:
@@ -12,8 +13,18 @@ def survey_form(request, hash):
     except:
         raise Http404("Survey does not exist")
     profile = schools_models.SchoolProfile.objects.get(pk=pk)
-    if profile.submitted_at and \
-            profile.submitted_at > datetime.datetime.now() + timedelta(days=1):
-        raise Http404("Survey Already Completed")
-    form = schools_forms.SchoolProfileForm()
+
+    if request.POST:
+        form = schools_forms.SchoolProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            survey = form.save(commit=False)
+            survey.school = profile.school
+            survey.save()
+            print("save")
+            print(survey.id)
+    else:
+        if profile.submitted_at and \
+                profile.submitted_at > timezone.now() + timedelta(days=1):
+            raise Http404("Survey Already Completed")
+        form = schools_forms.SchoolProfileForm(instance=profile)
     return render(request, 'survey_form.html', {'form': form})
