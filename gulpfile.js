12fe841365bd900d15ad88f011195dc238ea121c
Graham Whitley <lotusgraham@users.noreply.github.com>
2016-11-15 18:26:14 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,34 +12,3 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./frontend/sass/*.scss', ['sass']);
 });
-
-// var cssTask = function () {
-//     var lessOpts = {
-//       relativeUrls: true,
-//     };
-//     if (options.development) {
-//       var run = function () {
-//         var start = Date.now();
-//         console.log('Building CSS bundle');
-//         return gulp.src(options.css.src)
-//           .pipe(gulpif(options.development, livereload()))
-//           .pipe(concat('index.less'))
-//           .pipe(less(lessOpts))
-//           .pipe(rename('bundle.css'))
-//           .pipe(gulp.dest(options.css.dest))
-//           .pipe(notify(function () {
-//             console.log('CSS bundle built in ' + (Date.now() - start) + 'ms');
-//           }));
-//       };
-//       gulp.watch(options.css.watch, run);
-//       return run();
-//     } else {
-//       return gulp.src(options.css.src)
-//         .pipe(concat('index.less'))
-//         .pipe(less(lessOpts))
-//         .pipe(rename('bundle.css'))
-//         .pipe(cssmin())
-//         .pipe(gulp.dest(options.css.dest));
-//     }
-// };
-// gulp.task('css', cssTask);
