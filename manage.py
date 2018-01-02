a3d696001135e4ccc1c6ca792f39ead3e186b629
Colin Copeland <copelco@caktusgroup.com>
2018-01-02 16:12:48 -0500

    default to docker settings

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import dotenv
 dotenv.read_dotenv()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "school_navigator.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "school_navigator.settings.docker")
 
     from django.core.management import execute_from_command_line
 
