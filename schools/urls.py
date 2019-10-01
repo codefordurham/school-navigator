dfbe541b85c0775025ea3610af239ead946e07ef
Colin Copeland <copelco@caktusgroup.com>
2019-10-01 19:58:05 -0400

    more work on updating packages

--- schools/urls.py
+++ schools/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from schools.api import AllSchools, LocalSchools, SchoolDetail, SchoolProfileDetail
 
-school_urls = patterns('',
+school_urls = [
     url(r'detail/(?P<pk>[0-9]+)/$', SchoolDetail.as_view(), name='school-detail'),
     url(r'survey/(?P<pk>\w+)/$', SchoolProfileDetail.as_view(), name='school-survey'),
     url(r'local/$', LocalSchools.as_view(), name='local-schools'),
     url(r'$', AllSchools.as_view(), name='all-schools'),
-)
+]
